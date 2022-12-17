f22338f4eb49381c2352657aa8c941484e48a4c5
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2022-12-16 23:49:56 -0800

    hook for get firestore data / integrated firestore

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCMr-WjFqW8pX9WVBu0dwHan6QvOuOreg8",
+  authDomain: "tictactoe-8b46e.firebaseapp.com",
+  projectId: "tictactoe-8b46e",
+  storageBucket: "tictactoe-8b46e.appspot.com",
+  messagingSenderId: "621369256828",
+  appId: "1:621369256828:web:78095cdea12a1f28a692f9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+export default db;
---
+++ src/hooks/useFetchData.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { getDoc } from "firebase/firestore";
+
+export const useFetchData = (docRef) => {
+  const [documents, setDocuments] = React.useState();
+
+  const getData = async () => {
+    try {
+        const docSnap = await getDoc(docRef);
+        setDocuments(docSnap)
+    } catch(error) {
+        console.log(error)
+    }
+  }
+
+  React.useEffect(() => {
+    getData()
+  }, []);
+  return documents;
+};
