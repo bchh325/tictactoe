0dc44340938422fdca5dd49dc3d191ca501cfa11
bryanmtxd <67843182+bchh325@users.noreply.github.com>
2022-12-20 12:56:21 -0800

    update App.js

--- src/hooks/useFetchData.jsx
+++ src/hooks/useFetchData.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { getDoc } from "firebase/firestore";
+import { updateDoc } from "firebase/firestore";
 
 export const useFetchData = (docRef) => {
   const [documents, setDocuments] = React.useState();
 
   const getData = async () => {
     try {
-        const docSnap = await getDoc(docRef);
-        setDocuments(docSnap)
-    } catch(error) {
-        console.log(error)
+      const docSnap = await getDoc(docRef);
+      setDocuments(docSnap)
+    } catch (error) {
+      console.log(error)
     }
   }
 
